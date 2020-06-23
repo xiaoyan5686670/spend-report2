bfa8d33782d115c73d061f99ea5ce818282f8fea
xiaoyan5686670 <xiaoyan5686670@163.com>
2020-06-23 21:40:43 +0800

    first commit

--- src/main/java/sql/sqltest6.java
+++ src/main/java/sql/sqltest6.java
@@ -20,37 +20,69 @@ import static org.apache.flink.table.api.Expressions.$;
 
 
 public class sqltest6 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
         StreamExecutionEnvironment fsEnv = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
         StreamTableEnvironment fsTableEnv = StreamTableEnvironment.create(fsEnv, fsSettings);
 
         final Schema schema = new Schema().field("count", DataTypes.INT()).field("word",DataTypes.STRING());
 
-        fsTableEnv.connect(new FileSystem())
+        fsTableEnv.connect(new FileSystem().path("c:\\tmp\\qxy1.csv"))
                 .withFormat(new OldCsv().deriveSchema())
                 .withSchema(schema)
-                .createTemporaryTable("MySouce1");
+                .createTemporaryTable("MySource1");
 
-        fsTableEnv.connect(new FileSystem())
+        fsTableEnv.connect(new FileSystem().path("c:\\tmp\\qxy2.csv"))
                 .withFormat(new OldCsv().deriveSchema())
                 .withSchema(schema)
                 .createTemporaryTable("MySource2");
 
-        fsTableEnv.connect(new FileSystem())
+        fsTableEnv.connect(new FileSystem().path("c:\\tmp\\sink1"))
                 .withFormat(new OldCsv().deriveSchema())
                 .withSchema(schema)
                 .createTemporaryTable("MySink1");
 
-        fsTableEnv.connect(new FileSystem())
+        fsTableEnv.connect(new FileSystem().path("c:\\tmp\\sink2"))
                 .withFormat(new OldCsv().deriveSchema())
                 .withSchema(schema)
                 .createTemporaryTable("MySink2");
 
         Table table1= fsTableEnv.from("MySource1").where($("word").like("F%"));
-        table1.intersect("MySink1");
+        table1.insertInto("MySink1");
+
+        Table table2 = table1.unionAll(fsTableEnv.from("MySource2"));
+        table2.insertInto("MySink2");
+        String explanation = fsTableEnv.explain(false);
+        System.out.println(explanation);
+        fsTableEnv.execute("qxy");
 
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
