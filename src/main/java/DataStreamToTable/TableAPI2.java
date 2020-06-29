234d06786b0ff93c6822f75751075c358b20cea3
xiaoyan5686670 <xiaoyan5686670@163.com>
2020-06-29 22:12:02 +0800

    Convert a Table into a DataStream

--- src/main/java/DataStreamToTable/TableAPI2.java
+++ src/main/java/DataStreamToTable/TableAPI2.java
@@ -34,7 +34,7 @@ public class TableAPI2 {
                 .field("b", DataTypes.INT())
                 .field("c", DataTypes.BIGINT())
                 .field("rowtime",DataTypes.BIGINT()); //此处类型不能为：TIMESTAMP
-        tEnv.connect(new FileSystem().path("c:\\tmp\\qxy4.csv"))
+        tEnv.connect(new FileSystem().path("c:\\tmp\\qxy4.csv"))//CSV中的时间戳1592726259000L不用加L,否则报originated by LongParser: NUMERIC_VALUE_ILLEGAL_CHARACTER.
                 .withFormat(new OldCsv().fieldDelimiter("|").deriveSchema())
                 .withSchema(schema)
                 .createTemporaryTable("Orders");
@@ -47,7 +47,7 @@ public class TableAPI2 {
                 $("c").isNotNull()
         )).select($("a").lowerCase().as("a"),$("b"),$("rowtime"))
                 .window(Tumble.over(lit(1).hours()).on($("rowtime")).as("hourlyWindow"))
-                .groupBy($("hourlyWindow"),$("a"))
+                .groupBy($("hourlyWindow"),$("a")) //此处的"hourlyWinow为window函数中的定义的别名，要保持一致。
                 .select($("a"),$("hourlyWindow").end().as("hour"),
                         $("b").avg().as("avgBillingAmount"));
         //DataSet<Row> result2 = tEnv.toDataSet(result, Row.class);
