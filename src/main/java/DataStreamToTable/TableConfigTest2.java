9d4cbe208fcad746ce98463db9affb63f696fd19
xiaoyan5686670 <xiaoyan5686670@163.com>
2020-07-01 22:18:27 +0800

    Convert a Table into a DataStream

--- src/main/java/DataStreamToTable/TableConfigTest2.java
+++ src/main/java/DataStreamToTable/TableConfigTest2.java
@@ -3,9 +3,14 @@ package DataStreamToTable;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.GroupWindowedTable;
 import org.apache.flink.table.api.Table;
@@ -30,7 +35,7 @@ public class TableConfigTest2 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
-
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env,bsSettings);
 
         // register Orders table in table environment
@@ -40,11 +45,16 @@ public class TableConfigTest2 {
                 new Order("李志刚", "diaper", 4,1592726257000L),
                 new Order("常保平", "rubber", 2,1592726258000L)));
 // specify table program
-        tEnv.createTemporaryView("Orders", Orders);
-        Table orders = tEnv.from("Orders"); // schema (a, b, c, rowtime)
-        Table result  = orders.select($("user").lowerCase().as("a"),$("amount"),$("rowtime").rowtime());
-        GroupWindowedTable windowedTable = result.window(Tumble.over(lit(1).minutes()).on($("rowtime")).as("userActionWindow"));
-         windowedTable.groupBy($("userActionWindow"),$("user")).select($("amount").count()).execute().print();
+//        DataStream<Order> stream  =Orders.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Order>(Time.seconds(10)) {
+//            @Override
+//            public long extractTimestamp(Order order) {
+//                return order.rowtime;
+//            }
+//        });
+       tEnv.createTemporaryView("Orders", Orders, $("user"), $("product"),$("amount"), $("rowtime").rowtime());
+       Table orders = tEnv.from("Orders").select($("user").lowerCase().as("a"),$("amount"),$("rowtime").rowtime());
+       orders.window(Tumble.over(lit(1).minutes()).on($("rowtime")).as("userActionWindow")).groupBy($("userActionWindow"),$("user"))
+                .select($("user"),$("userActionWindow").end().as("hour"),$("amount").avg().as("avgBillingAmount")).execute().print();
 
 
     }
