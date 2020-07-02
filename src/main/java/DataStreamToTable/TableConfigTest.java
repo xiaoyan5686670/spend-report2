c9e87d517ec8fc90e1522884eb417b52bd7c0549
xiaoyan.qin <xiaoyan5686670@163.com>
2020-07-02 18:00:43 +0800

    first.

--- src/main/java/DataStreamToTable/TableConfigTest.java
+++ src/main/java/DataStreamToTable/TableConfigTest.java
@@ -1,10 +1,12 @@
 package DataStreamToTable;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.CoreOptions;
 import org.apache.flink.configuration.PipelineOptions;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.*;
@@ -30,7 +32,7 @@ import static org.apache.flink.table.api.Expressions.lit;
  **********************************/
 public class TableConfigTest {
     public static void main(String[] args) throws Exception {
-        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
+        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build();
         TableEnvironment tEnv = TableEnvironment.create(fsSettings);
 
         // obtain query configuration from TableEnvironment
@@ -49,23 +51,27 @@ public class TableConfigTest {
                 .field("b", DataTypes.INT())
                 .field("c", DataTypes.BIGINT())
                 .field("rowtime",DataTypes.BIGINT()); //此处类型不能为：TIMESTAMP
-        tEnv.connect(new FileSystem().path("d:\\tmp\\qxy4.csv"))//CSV中的时间戳1592726259000L不用加L,否则报originated by LongParser: NUMERIC_VALUE_ILLEGAL_CHARACTER.
+       tEnv.connect(new FileSystem().path("d:\\tmp\\qxy4.csv"))//CSV中的时间戳1592726259000L不用加L,否则报originated by LongParser: NUMERIC_VALUE_ILLEGAL_CHARACTER.
                 .withFormat(new OldCsv().fieldDelimiter("|").deriveSchema())
                 .withSchema(schema)
                 .createTemporaryTable("Orders");
 
+
+
         Table orders = tEnv.from("Orders"); // schema (a, b, c, rowtime)
 
-        orders.select($("a").lowerCase().as("a"),$("b"),$("rowtime").proctime())
+
+        orders.select($("a").lowerCase().as("a"),$("b"),$("rowtime"))
                 .window(Tumble.over(lit(1).hours()).on($("rowtime")).as("hourlyWindow"))
                 .groupBy($("hourlyWindow"),$("a")) //此处的"hourlyWinow为window函数中的定义的别名，要保持一致。
                 .select($("a"),$("hourlyWindow").end().as("hour"),
-                        $("b").avg().as("avgBillingAmount"));
+                        $("b").avg().as("avgBillingAmount")).execute().print();
         //DataSet<Row> result2 = tEnv.toDataSet(result, Row.class);
 
         // register TableSink
 
      //   result.execute().print();
         tEnv.execute("");
+
     }
 }
